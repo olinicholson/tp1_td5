421a91016fd858da3ef4987ff0895654f3545622
olinicholson <olinicholson4@gmail.com>
2024-09-16 00:18:02 -0300

    4 maquinas

--- tp1.cpp
+++ tp1.cpp
@@ -133,7 +133,8 @@ int prog_dinamica_2(size_t instancia, size_t m, vector<int>& gpus_solicitadas, v
         return memo[instancia][gpus_por_maquina1][gpus_por_maquina2];
     }
 
-    // Opción 1: No asignar la instancia actual a ninguna máquina
+
+        // Opción 1: No asignar la instancia actual a ninguna máquina
     int beneficio_no_asignar = prog_dinamica_2(instancia + 1, m, gpus_solicitadas, beneficios, 
                                                gpus_por_maquina1, gpus_por_maquina2, s, 
                                                max_beneficio, memo);
@@ -251,24 +252,106 @@ int prog_dinamica_3(size_t instancia, size_t m, vector<int>& gpus_solicitadas, v
 
 
 
+int prog_dinamica_4(size_t instancia, size_t m, vector<int>& gpus_solicitadas, vector<int>& beneficios, 
+                    int gpus_por_maquina1, int gpus_por_maquina2,int gpus_por_maquina3, int gpus_por_maquina4, vector<vector<int>>& s, 
+                    int& max_beneficio, vector<vector<vector<vector<vector<int>>>>>& memo) {
+    // Caso base: cuando hemos considerado todas las instancias
+    if (instancia == gpus_solicitadas.size()) {
+        return 0;  //el beneficio es 0
+    }
+
+    // Si ya hemos calculado este subproblema, devolver el resultado almacenado
+    if (memo[instancia][gpus_por_maquina1][gpus_por_maquina2] [gpus_por_maquina3][gpus_por_maquina4]!= -1) {
+        return memo[instancia][gpus_por_maquina1][gpus_por_maquina2][gpus_por_maquina3][gpus_por_maquina4];
+    }
+
+    // Opción 1: No asignar la instancia actual a ninguna máquina
+    int beneficio_no_asignar = prog_dinamica_4(instancia + 1, m, gpus_solicitadas, beneficios, 
+                                               gpus_por_maquina1, gpus_por_maquina2, gpus_por_maquina3,gpus_por_maquina4, s, 
+                                               max_beneficio, memo);
+
+    // Opción 2: Asignar la instancia a la máquina 1 si es posible
+    int beneficio_asignar_maquina1 = 0;
+    if (gpus_solicitadas[instancia] <= gpus_por_maquina1) {
+        beneficio_asignar_maquina1 = beneficios[instancia] + 
+            prog_dinamica_4(instancia + 1, m, gpus_solicitadas, beneficios, 
+                            gpus_por_maquina1 - gpus_solicitadas[instancia], gpus_por_maquina2, gpus_por_maquina3,gpus_por_maquina4, s, 
+                            max_beneficio, memo);
+    }
+
+    // Opción 3: Asignar la instancia a la máquina 2 si es posible
+    int beneficio_asignar_maquina2 = 0;
+    if (gpus_solicitadas[instancia] <= gpus_por_maquina2) {
+        beneficio_asignar_maquina2 = beneficios[instancia] + 
+            prog_dinamica_4(instancia + 1, m, gpus_solicitadas, beneficios, 
+                            gpus_por_maquina1, gpus_por_maquina2 - gpus_solicitadas[instancia], gpus_por_maquina3,gpus_por_maquina4,s, 
+                            max_beneficio, memo);
+    }
+
+     // Opción 4: Asignar la instancia a la máquina 3 si es posible
+    int beneficio_asignar_maquina3 = 0;
+    if (gpus_solicitadas[instancia] <= gpus_por_maquina3) {
+        beneficio_asignar_maquina3 = beneficios[instancia] + 
+            prog_dinamica_4(instancia + 1, m, gpus_solicitadas, beneficios, 
+                            gpus_por_maquina1, gpus_por_maquina2, gpus_por_maquina3  - gpus_solicitadas[instancia],gpus_por_maquina4,s, 
+                            max_beneficio, memo);
+    }
+
+     // Opción 4: Asignar la instancia a la máquina 4 si es posible
+    int beneficio_asignar_maquina4 = 0;
+    if (gpus_solicitadas[instancia] <= gpus_por_maquina4) {
+        beneficio_asignar_maquina4 = beneficios[instancia] + 
+            prog_dinamica_4(instancia + 1, m, gpus_solicitadas, beneficios, 
+                            gpus_por_maquina1, gpus_por_maquina2, gpus_por_maquina3 ,gpus_por_maquina4  - gpus_solicitadas[instancia],s, 
+                            max_beneficio, memo);
+    }
+
+    // El máximo entre las opciones
+    int mejor_beneficio = max({beneficio_no_asignar, beneficio_asignar_maquina1, beneficio_asignar_maquina2,  beneficio_asignar_maquina3, beneficio_asignar_maquina4});
+
+    // Guardamos el resultado 
+    memo[instancia][gpus_por_maquina1][gpus_por_maquina2][gpus_por_maquina3][gpus_por_maquina4]  = mejor_beneficio;
+
+    // Actualizamos el máximo beneficio encontrado
+    max_beneficio = max(max_beneficio, mejor_beneficio);
+
+    return mejor_beneficio;
+}
+
+
+
+
 
 //main con 3 maquinas
 int main() {
     int n = 5; // Número de instancias
-    int m = 3; // Número de máquinas
+    int m = 4; // Número de máquinas
     vector<int> beneficios = {10, 10, 10, 5, 5}; // Beneficios por instancia
     vector<int> gpus_solicitadas = {5, 5, 5, 5, 5}; // GPUs solicitadas por instancia
-    vector<int> gpus_por_maquina = {1, 1,13}; // GPUs disponibles por máquina 1 y 2
+    vector<int> gpus_por_maquina = {1, 1,13,13}; // GPUs disponibles por máquina 1 y 2
     vector<vector<int>> s(n, vector<int>(m, 0)); // Matriz de asignaciones
-
-    // Inicializamos memo: (instancia, GPUs máquina 1, GPUs máquina 3)    
-    vector<vector<vector<vector<int>>>> memo( n, vector<vector<vector<int>>>( gpus_por_maquina[0] + 1, vector<vector<int>>(
-            gpus_por_maquina[1] + 1, vector<int>(gpus_por_maquina[2] + 1, -1))));
+    int max_beneficio= 0;
+    // Inicializamos memo: (instancia, GPUs máquina 1, GPUs máquina 3)
+      
+    vector<vector<vector<vector<vector<int>>>>> memo(
+    n, vector<vector<vector<vector<int>>>>(  gpus_por_maquina[0] + 1, 
+        vector<vector<vector<int>>>(gpus_por_maquina[1] + 1, 
+        vector<vector<int>>(gpus_por_maquina[2] + 1,  
+        vector<int>(gpus_por_maquina[3] + 1, -1)) ) ));
     
+   /*
+    vector<vector<vector<vector<int>>>> memo(
+    n, vector<vector<vector<int>>>(  gpus_por_maquina[0] + 1, 
+        vector<vector<int>>(gpus_por_maquina[1] + 1, 
+        vector<int>(gpus_por_maquina[2] + 1,  
+         -1)) ) );
+
     int max_beneficio = 0;
+    */
+
 
     // Llamamos a la función de programación dinámica
-    prog_dinamica_3(0, m, gpus_solicitadas, beneficios, gpus_por_maquina[0], gpus_por_maquina[1],gpus_por_maquina[2], s, max_beneficio, memo);
+    prog_dinamica_4(0, m, gpus_solicitadas, beneficios, gpus_por_maquina[0], gpus_por_maquina[1],gpus_por_maquina[2],gpus_por_maquina[3],s, max_beneficio, memo);
 
     cout << "El beneficio maximo es: " << max_beneficio << endl;
 
